6f638117a141a49dc5b72589de4861a807fb117b
cinexin <migueguillamon@gmail.com>
2017-03-06 12:47:02 +0100

    Modulo 3 Ejercicio 2

---
+++ Modulo3/CicloVida/app/src/main/java/upv/ejercicios/Modulo3Ejercicio2/CicloVidaActivity.java
@@ -0,0 +1,35 @@
+package upv.ejercicios.Modulo3Ejercicio2;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.util.Log;
+import android.widget.TextView;
+
+public class CicloVidaActivity extends Activity {
+    StringBuilder mensajes = new StringBuilder();
+    TextView visor ;
+
+    private void log(String text) {
+        Log.d("Ciclo vida", text);
+        mensajes.append(text);
+        mensajes.append("\n");
+        visor.setText(mensajes.toString());
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        visor = new TextView(this);
+        visor.setTextSize(24);
+        visor.setText(mensajes.toString());
+        setContentView(visor);
+        log(getResources().getString(R.string.on_create_msg_log));
+
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        log(getResources().getString(R.string.on_resume_msg_log));
+    }
+}
