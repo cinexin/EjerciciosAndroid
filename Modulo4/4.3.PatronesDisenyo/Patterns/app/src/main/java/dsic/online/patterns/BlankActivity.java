cb3c9a6304ac634f62ea479a8e692637be3cdb2c
cinexin <migueguillamon@gmail.com>
2017-03-08 11:39:26 +0100

    Ejercicios Modulo 4.3 Patrones de diseño finalizados

--- Modulo4/4.3.PatronesDisenyo/Patterns/app/src/main/java/dsic/online/patterns/BlankActivity.java
+++ Modulo4/4.3.PatronesDisenyo/Patterns/app/src/main/java/dsic/online/patterns/BlankActivity.java
@@ -1,10 +1,40 @@
 package dsic.online.patterns;
 
 import android.os.Bundle;
-import android.support.v7.app.AppCompatActivity;
+import android.support.v7.app.ActionBarActivity;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.widget.TextView;
+import android.widget.Toast;
 
-public class BlankActivity extends AppCompatActivity {
+public class BlankActivity extends ActionBarActivity {
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_general_activity, menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.menu_search:
+                Toast.makeText(this,R.string.action_search,Toast.LENGTH_SHORT).show();
+                return true;
+            case R.id.menu_settings:
+                Toast.makeText(this,R.string.action_settings, Toast.LENGTH_SHORT).show();
+                return true;
+            case R.id.menu_share:
+                Toast.makeText(this, R.string.action_share, Toast.LENGTH_SHORT).show();
+                return true;
+
+            default:
+                return super.onOptionsItemSelected(item);
+
+        }
+
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
--- Modulo4/4.3.PatronesDisenyo/Patterns/app/src/main/java/dsic/online/patterns/DashboardActivity.java
+++ Modulo4/4.3.PatronesDisenyo/Patterns/app/src/main/java/dsic/online/patterns/DashboardActivity.java
@@ -2,12 +2,32 @@ package dsic.online.patterns;
 
 import android.content.Intent;
 import android.os.Bundle;
-import android.support.v7.app.AppCompatActivity;
+import android.support.v7.app.ActionBarActivity;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Toast;
 
 
-public class DashboardActivity extends AppCompatActivity {
+public class DashboardActivity extends ActionBarActivity {
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.menu_settings:
+                Toast.makeText(this, R.string.action_settings, Toast.LENGTH_SHORT).show();
+                return true;
+
+            default:
+                return super.onOptionsItemSelected(item);
+        }
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_dashboard, menu);
+        return super.onCreateOptionsMenu(menu);
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
